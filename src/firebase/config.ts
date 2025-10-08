88f919046ad42115dec86c9b96327bd8c2961f71
Retas Lintas Batas <retaslintasbatas@gmail.com>
2025-10-08 16:43:29 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-6945435693-50081",
+  "appId": "1:374721592233:web:17fb692021aaf38bc562b8",
+  "apiKey": "AIzaSyCWsRnos_botb96XjPOuTxCKvBqmjuimwE",
+  "authDomain": "studio-6945435693-50081.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "374721592233"
+};
