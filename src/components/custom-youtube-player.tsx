837b6ec4111c3d422e41ce95c5b0e35b2cef796a
Retas Lintas Batas <retaslintasbatas@gmail.com>
2025-10-10 03:43:02 +0000

    buat player di mobile pojoknya round

--- src/components/custom-youtube-player.tsx
+++ src/components/custom-youtube-player.tsx
@@ -21,7 +21,7 @@ export function CustomYouTubePlayer({ youtubeId }: CustomYouTubePlayerProps) {
   const videoUrl = `https://www.youtube.com/watch?v=${youtubeId}`;
 
   return (
-    <div className="relative w-full overflow-hidden aspect-video bg-black sm:rounded-xl">
+    <div className="relative w-full overflow-hidden aspect-video bg-black rounded-xl">
       {hasWindow ? (
         <>
           <ReactPlayer
